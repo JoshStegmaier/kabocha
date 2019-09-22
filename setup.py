94933e4728fac8d93b0e4e4910f80150a3ca3f1a
Josh Stegmaier <jrs@joshstegmaier.com>
2019-09-21 19:28:33 -0500

    Fixed issue with dist package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="kabocha",
-    version="1.0",
+    version="1.0.1",
     description="JSON error extension to tastypie ",
     long_description="Extension to django-tastypie to help deal with more errors directly with JSON",
     keywords="django, tastypie, json, errors, api, rest",
@@ -10,9 +10,9 @@ setuptools.setup(
     author_email="jrs@joshstegmaier.com",
     url="https://github.com/JoshStegmaier/kabocha/",
     license="MIT",
-    packages=["kabocha"],
+    packages=["kabocha", "kabocha.errors"],
     classifiers=[
-        "Programming Language :: Python 3",
+        "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Framework :: Django",
         "Environment :: Web Environment",
